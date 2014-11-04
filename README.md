22077e719efda4493824e62ce66fb304d7498968
Maigo Erit <maigo.erit@smit.ee>
2014-11-04 15:27:49 +0200

    create-plugin ScaffoldAngular removed unnecessary files. Added Scaffold-core dependency

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 grails-scaffold-angular
 =======================
+
+Using generator-angular-fullstack frontend as basis (https://www.npmjs.org/package/generator-angular-fullstack)
---
+++ ScaffoldAngularGrailsPlugin.groovy
@@ -0,0 +1,30 @@
+class ScaffoldAngularGrailsPlugin {
+    def version = "0.1"
+    // the version or versions of Grails the plugin is designed for
+    def grailsVersion = "2.4 > *"
+
+	def dependsOn = [scaffoldCore: "0.1"]
+	
+    def title = "Scaffold Angular Plugin" 
+    def author = "Maigo Erit"
+    def authorEmail = "maigo.erit@gmail.com"
+    def description = '''\
+Grails plugin for generating working demo with Angular frontend and REST backend.
+'''
+
+    // URL to the plugin's documentation
+    def documentation = "http://grails.org/plugin/scaffold-angular"
+
+    // Extra (optional) plugin metadata
+
+    // License: one of 'APACHE', 'GPL2', 'GPL3'
+//    def license = "APACHE"
+
+    // Online location of the plugin's browseable source code.
+    def scm = [ url: "https://github.com/MayGo/grails-scaffold-angular" ]
+
+
+    def doWithSpring = {
+		templatesLocator(grails.plugin.scaffold.core.DefaultTemplatesLocator, "scaffold-angular")
+	}
+}
