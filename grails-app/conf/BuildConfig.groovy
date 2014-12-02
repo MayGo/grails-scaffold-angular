19e516fde7128da8afc38849191100b8d643aace
Maigo Erit <maigo.erit@smit.ee>
2014-12-02 09:55:10 +0200

    def version = "0.3.2"

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -40,10 +40,9 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        build(":release:3.0.1",
-              ":rest-client-builder:1.0.3") {
+        build(":release:3.0.1") {
             export = false
         }
-		compile ':scaffold-core:0.2.3'
+		compile ':scaffold-core:0.2.4'
     }
 }
