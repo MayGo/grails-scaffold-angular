70dca6ac7c37d95305e231018a582d600dbb8eb9
Maigo Erit <maigo.erit@smit.ee>
2015-02-04 12:37:39 +0200

    session.service.js template parse fix

--- src/templates/scaffold/frontendAngular/dynamic/client/app/session/session.service.js
+++ src/templates/scaffold/frontendAngular/dynamic/client/app/session/session.service.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('angularDemoApp').provider('SessionService', function ($authProvider, appConfig) {
+angular.module('angularDemoApp').provider('SessionService', function (\$authProvider, appConfig) {
 
 	\$authProvider.loginOnSignup = true;
     \$authProvider.logoutRedirect = '/';
@@ -10,54 +10,54 @@ angular.module('angularDemoApp').provider('SessionService', function ($authProvi
     \$authProvider.tokenName =  'access_token';
     \$authProvider.authHeader =  'Authorization';
 
-	this.$get = function ($localStorage, $auth, $q, appConfig, $http) {
-    var service = {};
-    var _currentUser = {};
+	this.\$get = function (\$localStorage, \$auth, \$q, appConfig, \$http) {
+		var service = {};
+		var _currentUser = {};
 
-    service.login = function (username, password) {
-      var deferred = $q.defer();
+		service.login = function (username, password) {
+		  var deferred = \$q.defer();
 
-	  \$auth.login({ username: username, password: password }).then(function (response) {
-        console.log("Logged in.");
-        service.afterLogin(response.data);
-        deferred.resolve(response.data);
-      }).catch(function (response) {
-        console.log("Error when login.");
-        deferred.reject(response)
-      });
+		  \$auth.login({ username: username, password: password }).then(function (response) {
+			console.log("Logged in.");
+			service.afterLogin(response.data);
+			deferred.resolve(response.data);
+		  }).catch(function (response) {
+			console.log("Error when login.");
+			deferred.reject(response)
+		  });
 
-      return deferred.promise;
-    };
+		  return deferred.promise;
+		};
 
-    service.afterLogin = function (userData) {
-      _currentUser = userData;
-      // save settings to local storage
-      $localStorage.userData = userData;
-    };
+		service.afterLogin = function (userData) {
+		  _currentUser = userData;
+		  // save settings to local storage
+			\$localStorage.userData = userData;
+		};
 
-    service.logout = function () {
-      delete $localStorage.userData;
-      _currentUser = {};
-	  return $auth.logout();
-    };
+		service.logout = function () {
+		  delete \$localStorage.userData;
+		  _currentUser = {};
+		  return \$auth.logout();
+		};
 
-    service.getCurrentUser = function () {
-      if (!_.isEmpty(_currentUser)) {
-        return _currentUser;
-      }
+		service.getCurrentUser = function () {
+		  if (!_.isEmpty(_currentUser)) {
+			return _currentUser;
+		  }
 
-      //get from local storage
-      if (angular.isDefined($localStorage.userData)) {
-        _currentUser = $localStorage.userData;
-      }
+		  //get from local storage
+		  if (angular.isDefined(\$localStorage.userData)) {
+			_currentUser = \$localStorage.userData;
+		  }
 
-      return _currentUser;
-    };
+		  return _currentUser;
+		};
 
-    service.isAuthenticated = function () {
-      return !_.isEmpty(service.getCurrentUser())
-    };
+		service.isAuthenticated = function () {
+		  return !_.isEmpty(service.getCurrentUser())
+		};
 
-    return service;
-  };
+		return service;
+	};
 });
