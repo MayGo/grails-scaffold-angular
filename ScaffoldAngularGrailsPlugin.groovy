7e9a4e5d7e091c4938aa59e2b1826aa72b76f5b3
Maigo Erit <maigo.erit@smit.ee>
2015-02-18 14:52:06 +0200

    0.4.34

--- ScaffoldAngularGrailsPlugin.groovy
+++ ScaffoldAngularGrailsPlugin.groovy
@@ -2,7 +2,7 @@ import grails.plugin.scaffold.core.ConfigUtility
 import grails.buildtestdata.DomainInstanceBuilder
 
 class ScaffoldAngularGrailsPlugin {
-    def version = "0.4.33"
+    def version = "0.4.34"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.4 > *"
 
