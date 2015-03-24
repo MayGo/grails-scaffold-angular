57324c8f4fb00f6006e0bc0fa825c4d97fdbbbc8
Maigo Erit <maigo.erit@smit.ee>
2015-03-24 16:55:48 +0200

    use todays date when testing

--- src/templates/scaffold/backendRestTest/dynamic/functional/__packageName__/__shortName__Spec.groovy
+++ src/templates/scaffold/backendRestTest/dynamic/functional/__packageName__/__shortName__Spec.groovy
@@ -58,7 +58,7 @@ isComposite = DomainHelper.isComposite(domainClass)
 //buildTestDataService = grails.util.Holders.getApplicationContext().getBean('buildTestDataService')
 
 private renderAll(def dClass, boolean isResp = false, int groupId){
-	
+
 	String resp = ""
 	def inst = DomainHelper.createOrGetInst(dClass, groupId)
 	if(inst) resp += createDomainInstanceJson(dClass, isResp, inst)
@@ -68,22 +68,22 @@ private renderAll(def dClass, boolean isResp = false, int groupId){
 
 private String createDomainInstanceJson(def dClass, boolean isResp, def inst, List alreadyCreatedClasses = []){
 	alreadyCreatedClasses << dClass.name
-	
+
 	String respStr = ""
 	def properties = scaffoldingHelper.getProps(dClass).findAll{p->!p.isManyToMany() && !p.isOneToMany()}
 
 	properties.each{p->
 		String str = (isResp)?"\t\t\tresponse.json.":"\t\t\t\t"
 		String asign = (isResp)?"==":"="
-		
+
 		if(DomainClassArtefactHandler.isDomainClass(p.type) && !p.isEmbedded()){
 			def refClass = new DefaultGrailsDomainClass(p.type)
 			/*if(!alreadyCreatedClasses.contains(refClass.name)){
 				alreadyCreatedClasses << refClass.name
 				//def refClass = (p.referencedDomainClass)?p.referencedDomainClass:p.getReferencedPropertyType()
-				
+
 				//check if composite id
-				
+
 				if(isResp){
 					//str +="${p.name}?.id $asign $val\n"
 				}else{
@@ -104,14 +104,14 @@ private String createDomainInstanceJson(def dClass, boolean isResp, def inst, Li
 				}
 			}
 
-			
+
 			if(isResp){
 				str +="${p.name}?.id $asign $val\n"
 			}else{
 				str +="${p.name} $asign $val\n"
 			}
 			respStr += str
-			
+
 		} else if(p.isEmbedded()){
 
 			def refClass = p.component
@@ -131,14 +131,10 @@ private String createDomainInstanceJson(def dClass, boolean isResp, def inst, Li
 				if(p.type == boolean || p.type == Boolean) val = true
 				str +="${p.name} $asign $val\n"
 			}else if(p.type == Date || p.type == java.sql.Date || p.type == java.sql.Time || p.type == Calendar){
-				def inputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSZ")
-				def outputFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
-				outputFormat.timeZone = java.util.TimeZone.getTimeZone( 'GMT' )
 
 				String dateStr = (val)?'getTodayForInput()':''
-				
 				if(isResp){//format input differently then comparison
-					dateStr = (val)? 'getTodayForOutput()':''
+					dateStr = (val)? 'getTodayForOutput().toLong()':''
 				}
 				str +="${p.name} $asign $dateStr\n"
 			}else if(p.type == Map || "${p.type.name}" == "com.google.gson.internal.LinkedTreeMap"){
@@ -163,7 +159,7 @@ private String createDomainInstanceJson(def dClass, boolean isResp, def inst, Li
 			}
 			respStr += str
 		}
-		
+
 	}
 	return respStr
 }
@@ -171,18 +167,18 @@ private String createDomainInstanceJson(def dClass, boolean isResp, def inst, Li
 class ${className}Spec extends Specification implements RestQueries, AuthQueries, TestUtils{
 
 	String REST_URL = "\${APP_URL}/${shortNameLower}"
-	
+
 	@Shared
 	Long domainId
 	@Shared
 	Long otherDomainId
-	
+
 	@Shared
 	def authResponse
-	
+
 	@Shared
 	def response
-	
+
 	def setupSpec() {
 		authResponse = sendCorrectCredentials(APP_URL)
 	}
@@ -195,7 +191,7 @@ class ${className}Spec extends Specification implements RestQueries, AuthQueries
 			<%if(!isComposite){%>
 			otherDomainId = response.json.id
 			<%}%>
-			
+
 		then: 'Should create and return created values'
 <%renderAll(domainClass, true, 3)%>\
 			response.status == HttpStatus.CREATED.value()
@@ -209,27 +205,27 @@ class ${className}Spec extends Specification implements RestQueries, AuthQueries
 			<%if(!isComposite){%>
 			domainId = response.json.id
 			<%}%>
-			
+
 		then: 'Should create and return created values'
-			
+
 <%renderAll(domainClass, true, 1)%>\
 			response.status == HttpStatus.CREATED.value()
 	}
-	
-	
-			
-		
+
+
+
+
 
 	void 'Test reading ${className} instance.'() {
 		when: 'Read ${propertyName}'
 			response = readDomainItemWithParams(domainId.toString(), "")
 		then: 'Should return correct values'
-			
+
 <%renderAll(domainClass, true, 1)%>\
 			response.status == HttpStatus.OK.value()
 	}
-	
-	
+
+
 	void 'Test excluding fields from reading ${className} instance.'() {
 		when: 'Read ${propertyName} id excluded'
 			response = readDomainItemWithParams(domainId.toString(), 'excludes=id')
@@ -237,8 +233,8 @@ class ${className}Spec extends Specification implements RestQueries, AuthQueries
 			response.json.id == null
 			response.status == HttpStatus.OK.value()
 	}
-	
-	
+
+
 	void 'Test including fields from reading ${className} instance.'() {
 		when: 'Read ${propertyName} id excluded and then included'
 			response = readDomainItemWithParams(domainId.toString(), 'excludes=id&includes=id')
@@ -246,8 +242,8 @@ class ${className}Spec extends Specification implements RestQueries, AuthQueries
 			response.json.id != null
 			response.status == HttpStatus.OK.value()
 	}
-	
-	
+
+
 	void 'Test reading unexisting ${className} instance.'() {
 		when: 'Find unexisting ${propertyName}'
 			response = readDomainItemWithParams('9999999999', '')
@@ -259,7 +255,7 @@ class ${className}Spec extends Specification implements RestQueries, AuthQueries
 			response.status == HttpStatus.UNPROCESSABLE_ENTITY.value()
 	}
 
-	
+
 	void 'Test updating ${className} instance.'() {
 		when: 'Update ${propertyName}'
 			response = sendUpdateWithData(domainId.toString()){
@@ -277,7 +273,7 @@ class ${className}Spec extends Specification implements RestQueries, AuthQueries
 			}
 		then: 'Should not find'
 			response.status == HttpStatus.NOT_FOUND.value()
-			
+
 		when: 'Update unexisting ${propertyName} id not a number'
 			response = sendUpdateWithData('nonexistent'){
 	<%renderAll(domainClass, false, 2)%>
@@ -285,7 +281,7 @@ class ${className}Spec extends Specification implements RestQueries, AuthQueries
 		then: 'Should not find'
 			response.status == HttpStatus.UNPROCESSABLE_ENTITY.value()
 	}
-	
+
 	void 'Test ${className} list sorting.'() {
 		when: 'Get ${propertyName} sorted list DESC'
 			response = queryListWithParams('order=desc&sort=id')
@@ -293,7 +289,7 @@ class ${className}Spec extends Specification implements RestQueries, AuthQueries
 		then: 'First item should be just inserted object'
 			response.json[0].id == domainId
 			response.status == HttpStatus.OK.value()
-		
+
 		when: 'Get ${propertyName} sorted list ASC'
 			response = queryListWithParams('order=asc&sort=id')
 
@@ -301,8 +297,8 @@ class ${className}Spec extends Specification implements RestQueries, AuthQueries
 			response.json[0].id != domainId
 			response.status == HttpStatus.OK.value()
 	}
-	
-	
+
+
 	void 'Test ${className} list max property query 2 items.'() {
 		when: 'Get ${propertyName} list with max 2 items'
 			response = queryListWithParams('max=2')
@@ -310,35 +306,35 @@ class ${className}Spec extends Specification implements RestQueries, AuthQueries
 		then: 'Should be only 2 items'
 			response.json.size() == 2
 	}
-	
-	
+
+
 	<%
 	domainClasses.first().getClazz().withTransaction{
 	if(domainClasses.first().getClazz().count()<= 100){%>@Ignore<%}}%> // have to have more then maxLimit items
 	void 'Test ${className} list max property.'() {
 		given:
 			int maxLimit = 100// Set real max items limit
-			
+
 		when: 'Get ${propertyName} list without max param'
 			response = queryListWithParams('')
 
 		then: 'Should return default maximum items'
 			response.json.size() == 10
-			
+
 		when: 'Get ${propertyName} list with maximum items'
 			response = queryListWithParams("max=\$maxLimit")
 
 		then: 'Should contains maximum items'
 			response.json.size() == maxLimit
-			
+
 		when: 'Get ${propertyName} list with maximum + 1 items'
 			response = queryListWithParams("max=\${maxLimit+1}")
 
 		then: 'Should contains maximum items'
 			response.json.size() == maxLimit
 	}
-	
-	
+
+
 	void 'Test excluding fields in ${className} list.'() {
 		when: 'Get ${propertyName} sorted list'
 			response = queryListWithParams('excludes=id')
@@ -346,8 +342,8 @@ class ${className}Spec extends Specification implements RestQueries, AuthQueries
 		then: 'First item should be just inserted object'
 			response.json[0].id == null
 	}
-	
-	
+
+
 	void 'Test including fields in ${className} list.'() {
 		when: 'Get ${propertyName} sorted list'
 			response = queryListWithParams('excludes=id&includes=id')
@@ -395,7 +391,7 @@ class ${className}Spec extends Specification implements RestQueries, AuthQueries
 			def instBefore = DomainHelper.createOrGetInst(domainClass, 1)//comparing instBefore and inst variables decides if test results 1 or 10 items in output
 			def inst = DomainHelper.createOrGetInst(domainClass, 2)
 			%>
-			
+
 		expect:
 			response.json.size() == respSize
 		where:
@@ -403,12 +399,15 @@ class ${className}Spec extends Specification implements RestQueries, AuthQueries
 			'{}'                || 10
 <% if(instBefore && inst)simpleProps.each{p->
 		def hasChanged = !(instBefore."${p.name}" == inst."${p.name}")
-		if(p.type == Date || p.type == java.sql.Date || p.type == java.sql.Time || p.type == Calendar){
-			hasChanged = false
-		}
+
 		def val = inst."${p.name}"
 		def realVal = DomainHelper.getRealValue(p, val)
 		def jsonVal
+		if(p.type == Date || p.type == java.sql.Date || p.type == java.sql.Time || p.type == Calendar){
+			hasChanged = false
+			realVal = (val)?'\' + getTodayForInput() + \'':''
+		}
+
 		if(p.referencedDomainClass?.hasProperty('id')){
 			def refClass = new DefaultGrailsDomainClass(p.type)
 
@@ -435,18 +434,18 @@ class ${className}Spec extends Specification implements RestQueries, AuthQueries
 		}
 }%>
 	}
-	
-	
-	
-	
+
+
+
+
 	void "Test deleting other ${className} instance."() {//This is for creating some data to test list sorting
 		when: "Delete ${propertyName}"
 			response = deleteDomainItem(otherDomainId.toString())
 		then:
 			response.status == HttpStatus.NO_CONTENT.value()
 	}
-	
-	
+
+
 	void "Test deleting ${className} instance."() {
 		when: "Delete ${propertyName}"
 			response = deleteDomainItem(domainId.toString())
