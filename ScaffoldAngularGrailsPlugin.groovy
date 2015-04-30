efa8ec484295729900eb1b0b084a47de67fe031f
Maigo Erit <maigo.erit@smit.ee>
2015-04-30 10:05:53 +0300

    1.2.3

--- ScaffoldAngularGrailsPlugin.groovy
+++ ScaffoldAngularGrailsPlugin.groovy
@@ -2,7 +2,7 @@ import grails.plugin.scaffold.core.ConfigUtility
 import grails.plugin.scaffold.core.DefaultTemplatesLocator
 
 class ScaffoldAngularGrailsPlugin {
-	def version = "1.2.2"
+	def version = "1.2.3"
 	def grailsVersion = "2.4 > *"
 	def loadAfter = ['scaffoldCore']
 	def loadBefore = ['buildTestData']
