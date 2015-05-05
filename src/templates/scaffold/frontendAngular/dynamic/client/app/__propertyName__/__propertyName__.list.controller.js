8926cd824d0c5d1f2cc99e2489376bd47359a10b
Maigo Erit <maigo.erit@smit.ee>
2015-05-05 13:56:25 +0300

    Loading overlay on list.

--- src/templates/scaffold/frontendAngular/dynamic/client/app/__propertyName__/__propertyName__.list.controller.js
+++ src/templates/scaffold/frontendAngular/dynamic/client/app/__propertyName__/__propertyName__.list.controller.js
@@ -2,73 +2,72 @@
 
 angular.module('angularDemoApp')
 	.controller('${domainClass.shortName}ListController', function (\$scope, \$rootScope,
-		\$state, \$q, ${domainClass.shortName}Service, \$stateParams, \$timeout, inform, ngTableParams, appConfig) {
+		\$state, \$q, ${domainClass.shortName}Service, \$stateParams, \$timeout, inform, ngTableParams, appConfig, \$location) {
 
-	if(\$state.current.data){
-		\$scope.isTab = \$state.current.data.isTab;
-	}
-
-	\$scope.delete${domainClass.shortName} = function(instance){
-		return ${domainClass.shortName}Service.deleteInstance(instance).then(function(instance){
-			\$scope.tableParams.reload();
-			return instance;
-		});
-	};
-
-
-	\$scope.search = {};
-
-	var filterTimeout;
-	\$scope.tableParams = new ngTableParams({
-		page: 1,            // show first page
-		count: 10,          // count per page
-		sorting: {
-			id: 'asc'     // initial sorting
-		},
-		filter: \$scope.search
-	}, {
-		total: 0,           // length of data
-		getData: function(\$defer, params) {
-
-			// do not let to make do much queries
-			if (filterTimeout){
-				\$timeout.cancel(filterTimeout);
-			}
-
-			filterTimeout = \$timeout(function() {
-				var offset = (params.page()-1) * params.count();
-				var paging = {max: appConfig.itemsByPage, offset: offset};
-
-				var query = _.merge(paging, params.filter())
-
-				if (params.sorting()) {
-					query.sort = Object.keys(params.sorting())[0];
-					query.order = params.sorting()[query.sort];
-				}
-
-
-				if(\$stateParams.relationName && \$stateParams.id){
-					if(_.isEmpty(query[\$stateParams.relationName])){
-						query[\$stateParams.relationName] = [];
-					}
-					query[\$stateParams.relationName].push(Number(\$stateParams.id));
-				}
-
-				var errorCallback = function(response){
-					if (response.data && response.data.errors) {
-						angular.forEach(response.data.errors, function (error) {
-							inform.add(error.message, {ttl: -1,'type': 'warning'});
-						});
-					}
-				};
-
-				${domainClass.shortName}Service.query(query, function(response, responseHeaders){
-					params.total(responseHeaders().total);
-					\$defer.resolve(response);
-				}, errorCallback);
-			}, 255);
-
+		if(\$state.current.data){
+			\$scope.isTab = \$state.current.data.isTab;
 		}
-	});
 
+		\$scope.delete${domainClass.shortName} = function(instance){
+			return ${domainClass.shortName}Service.deleteInstance(instance).then(function(instance){
+				\$scope.tableParams.reload();
+				return instance;
+			});
+		};
+
+
+		\$scope.search = {};
+
+		var filterTimeout;
+		var defaultParams = {
+			page: 1,            // show first page
+			count: appConfig.itemsByPage,          // count per page
+			sorting: {
+				id: 'asc'     // initial sorting
+			},
+			filter: \$scope.search
+		};
+		var parameters = angular.extend(defaultParams,\$location.search());
+		var errorCallback = function(response){
+			if (response.data && response.data.errors) {
+				angular.forEach(response.data.errors, function (error) {
+					inform.add(error.message, {ttl: -1,'type': 'warning'});
+				});
+			}
+		};
+		var settings = {
+			getData: function(\$defer, params) {
+				// do not let to make do much queries
+				if (filterTimeout){
+					\$timeout.cancel(filterTimeout);
+				}
+
+				filterTimeout = \$timeout(function() {
+					\$location.search(params.url()); // put params in url
+					var offset = (params.page()-1) * params.count();
+					var paging = {max: params.count(), offset: offset};
+
+					var query = _.merge(paging, params.filter())
+
+					if (params.sorting()) {
+						query.sort = Object.keys(params.sorting())[0];
+						query.order = params.sorting()[query.sort];
+					}
+
+					if(\$stateParams.relationName && \$stateParams.id){
+						if(_.isEmpty(query[\$stateParams.relationName])){
+							query[\$stateParams.relationName] = [];
+						}
+						query[\$stateParams.relationName].push(Number(\$stateParams.id));
+					}
+
+					${domainClass.shortName}Service.query(query, function(response, responseHeaders){
+						params.total(responseHeaders().total);
+						\$defer.resolve(response);
+					}, errorCallback);
+				}, 255);
+			}
+		};
+
+		\$scope.tableParams = new ngTableParams(parameters, settings);
 });
