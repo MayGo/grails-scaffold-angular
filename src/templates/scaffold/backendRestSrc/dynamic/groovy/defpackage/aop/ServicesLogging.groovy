8811617ede006395c0b519bf11c205c48503ec4e
Maigo Erit <maigo.erit@smit.ee>
2015-05-07 15:29:48 +0300

    ServiceLogging poincut uses packages commonPrefix

--- src/templates/scaffold/backendRestSrc/dynamic/groovy/defpackage/aop/ServicesLogging.groovy
+++ src/templates/scaffold/backendRestSrc/dynamic/groovy/defpackage/aop/ServicesLogging.groovy
@@ -9,36 +9,42 @@ import org.springframework.stereotype.Component
 @Component
 @Aspect
 class ServicesLogging {
+	<%
 
-	@Pointcut('bean(*Service) && execution(public * *(..))')
+	import org.apache.commons.lang3.StringUtils
+	String[] packages = domainClasses*.packageName
+	String pack = StringUtils.getCommonPrefix(packages)
+
+	%>
+	@Pointcut('bean(*Service) && execution(public * $pack.*(..))')
 	@SuppressWarnings(['EmptyMethod', 'SpaceAfterOpeningBrace'])
 	void publicServiceMethod() {}
 
-	@Pointcut('execution(public groovy.lang.MetaClass getMetaClass()) \
-				|| execution(public void setMetaClass(groovy.lang.MetaClass)) \
-				|| execution(public Object getProperty(String)) \
-				|| execution(public void setProperty(String, Object)) \
+	@Pointcut('execution(public groovy.lang.MetaClass getMetaClass()) \\
+				|| execution(public void setMetaClass(groovy.lang.MetaClass)) \\
+				|| execution(public Object getProperty(String)) \\
+				|| execution(public void setProperty(String, Object)) \\
 				|| execution(public Object invokeMethod(String, Object))')
 	@SuppressWarnings(['SpaceAfterOpeningBrace', 'EmptyMethod'])
 	void groovyObjectMethods() {}
 
-	@Pointcut('execution(public * *$*(..))')
+	@Pointcut('execution(public * *\$*(..))')
 	@SuppressWarnings(['EmptyMethod', 'SpaceAfterOpeningBrace'])
 	void groovyDollarSignMethods() {}
 
 	@Around('publicServiceMethod() && !groovyObjectMethods() && !groovyDollarSignMethods()')
 	@SuppressWarnings('CatchThrowable')
 	def traceServiceMethodCall(ProceedingJoinPoint jp) throws Throwable {
-		String invocationDescription = "method '$jp.signature.name' of class [$jp.signature.declaringTypeName]"
+		String invocationDescription = "method '\$jp.signature.name' of class [\$jp.signature.declaringTypeName]"
 		Object[] args = jp.args
 		String argsString = args*.toString().join(', ')
-		log.error "Entering $invocationDescription with args: $argsString"
+		log.debug "Entering \$invocationDescription with args: \$argsString"
 		try {
 			def result = jp.proceed()
-			log.debug "Exiting $invocationDescription"
+			log.debug "Exiting \$invocationDescription"
 			return result
 		} catch (Throwable e) {
-			log.debug "Exception thrown in $invocationDescription", e
+			log.debug "Exception thrown in \$invocationDescription", e
 			throw e
 		}
 	}
